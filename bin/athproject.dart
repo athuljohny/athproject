8cd17aa24749e407350edc9d410f77aba1f1e379
athuljohny <athuljohny123@gmail.com>
2022-09-19 03:38:59 -0700

    first commit

---
+++ bin/athproject.dart
@@ -0,0 +1,5 @@
+import 'package:athproject/athproject.dart' as athproject;
+
+void main(List<String> arguments) {
+  print('Hello world: ${athproject.calculate()}!');
+}
---
+++ bin/main.dart
@@ -0,0 +1,13 @@
+import 'dart:io';
+
+void main() {
+  String name =   "Anna";
+  int age     =     20;
+  double mark =     9.5;
+  int phone  = 77889966;
+  String email = 'athuljohny123@gmail.com';
+  
+  
+  stdout.write("my mark percentage is $mark");
+  stdout.write("my phone is $phone");
+}

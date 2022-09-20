51307e8e4f5af7068d4d31b6f15288e594b4c145
athuljohny <athuljohny123@gmail.com>
2022-09-20 03:54:18 -0700

    YES

---
+++ bin/abc.dart
@@ -0,0 +1,10 @@
+import 'dart:io';
+
+void main() {
+String name = 'tom';
+int age = 24;
+
+stdout.write("my name is $name");
+stdout.write("my age is $age");
+  
+}
---
+++ bin/class.dart
@@ -0,0 +1,49 @@
+class Car{
+  ///instance variable
+
+  String? color;
+  late int year;
+  double? mileage;
+
+  ///static variable
+  static String brand = "maruti";
+
+  void show(){
+    ///local variable
+    String engine = "petrol";
+    print("engine is $engine");
+    
+  }
+
+}
+void main(){
+  ///object creation => classname objectname = classname();
+  Car alto = Car();
+  print("color is ${alto.color = "Grey"}");
+  print("year is ${alto.year = 2005}");
+  print("car mileage is ${alto.mileage = 16}");
+  print("alto is a model of ${Car.brand}");
+  alto.show();
+
+  print("********************");
+
+  Car swift = Car();
+  print("color is ${swift.color = "red"}");
+  print("year is ${swift.year = 2010})");
+  print("mileage is ${swift.mileage = 20}");
+  print("color is ${swift.color ="green"}");
+  print("alto is a model of ${Car.brand}");
+  swift.show();
+
+  print("**********************");
+
+
+  Car hyundai = Car();
+  print ("color is ${hyundai.color ="black"}");
+  print("year is ${hyundai.year = 2009}");
+  print("mileage is ${hyundai.mileage = 20}");
+  print("hyunai is a model of ${Car.brand = "i20nline"}");
+  hyundai.show();
+
+
+}

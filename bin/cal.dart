909aedcfec902c5c765d52644651a14f1f068897
athuljohny <athuljohny123@gmail.com>
2022-09-21 03:13:58 -0700

    yes

---
+++ bin/cal.dart
@@ -0,0 +1,31 @@
+void sum()
+{
+  int a = 50 , b= 70;
+  print("sum is ${a+b}");
+}
+void sub()
+{
+  int a = 50 , b =70;
+  
+  print("difference is ${a-b}");
+  }
+
+  void mul()
+  {
+    int a = 30 , b =40;
+    print("product is ${a*b}");
+
+  }
+
+  void div()
+  {
+    int a = 12 , b =34;
+    print("div is ${a/b}");
+  }
+  void main()
+  {
+    void sum();
+    void sub();
+    void mul();
+    void div();
+  }
---
+++ bin/cal1.dart
@@ -0,0 +1,20 @@
+void add(int a, int b){  /// a & b are parameters /arguments / formal parameters
+   print('sum = ${a+b}');
+}
+void sub(int a, int b){
+  print('diff = ${a-b}');
+}
+void mul(int a, int b){
+  print('sum = ${a*b}');
+}
+void div(int a, int b){
+  print('diff = ${a/b}');
+}
+
+void main(){
+  add(45,60);
+  add(30, 45);
+  sub(41,29);
+  mul(92,57);
+  div(34,70);
+}

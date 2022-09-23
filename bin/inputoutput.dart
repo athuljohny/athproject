f78ab0010e93b9ff02e4640f95bfbba48869b4a5
athuljohny <athuljohny123@gmail.com>
2022-09-23 09:36:36 -0700

    yes

--- bin/inputoutput.dart
+++ bin/inputoutput.dart
@@ -1,8 +1,13 @@
 import "dart:io";
 void main(){
    print("enter your name");
-  String? name = stdin.readLineSync();
-print("hello $name");
+   print("enter your age");
+
+
+ String? name = stdin.readLineSync();
+  print("hello $name");
+int? age = int.parse(stdin.readLineSync()!);
+print("my $age");
 
 
 
